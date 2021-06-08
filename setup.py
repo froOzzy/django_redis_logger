7c75ab01e4f72d5dce77197538c6499ce950070d
froOzzy <53941414+froOzzy@users.noreply.github.com>
2021-06-08 21:30:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from os.path import join, dirname
 setup(
     name='django_redis_logger',
     version='1.0',
-    packages=['django_redis_logger'],
+    packages=['django-redis-logger'],
     long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
